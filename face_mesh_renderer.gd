3c1153432c9f9e8673cd09c58c1a7e157cb97d6e
DrHaid <40427630+DrHaid@users.noreply.github.com>
2022-12-21 17:10:32 +0100

    reuse instantiated spheres

--- face_mesh_renderer.gd
+++ face_mesh_renderer.gd
@@ -1,14 +1,19 @@
 extends Node
 
-func _on_UDPClient_face_mesh_received(points):
+var debug_points = []
+
+func _on_UDPClient_face_mesh_received(positions):
+	if len(debug_points) != len(positions):
+		init_points(len(positions))
+	for i in range(len(debug_points)):
+		debug_points[i].transform.origin = positions[i]
+
+func init_points(count: int):
 	for child in get_children():
 		child.queue_free()
 	var dot = load("res://debug_point.tscn")
-	var index = 0;
-	for point in points:
+	for i in range(count):
 		var dot_instance = dot.instance()
-		dot_instance.set_name(str(index))
-		dot_instance.transform.origin = point
-		# TODO bind signal for outputting landmarks
+		dot_instance.set_name(str(i))
+		debug_points.append(dot_instance)
 		add_child(dot_instance)
-		index += 1
--- udp_client.gd
+++ udp_client.gd
@@ -8,26 +8,28 @@ var socketUDP = PacketPeerUDP.new()
 func _ready():
 	start_client()
 
-func _process(_delta):
-	if socketUDP.get_available_packet_count() > 0:
-		var points: PoolVector3Array = []
-		var stream = StreamPeerBuffer.new()
-		stream.data_array = socketUDP.get_packet()
-		var batch_size = stream.get_float()
-		for _i in range(batch_size):
-			var x = stream.get_float()
-			var y = stream.get_float()
-			var z = stream.get_float()
-			points.append(Vector3(x, y, z))
-		
-		emit_signal("face_mesh_received", points)
-
 func _exit_tree():
 	socketUDP.close()
+	
+func _process(_delta):
+	if socketUDP.get_available_packet_count() > 0:
+		var stream = StreamPeerBuffer.new()
+		stream.data_array = socketUDP.get_packet()
+		var points = get_points(stream)
+		emit_signal("face_mesh_received", points)
 
 func start_client():
 	if (socketUDP.listen(PORT_CLIENT) != OK):
 		printt("An error occurred while attempting to listening on port: %s" % PORT_CLIENT)
 	else:
 		printt("Listening on port: %s" % PORT_CLIENT)
-
+		
+func get_points(stream: StreamPeerBuffer) -> PoolVector3Array:
+	var points: PoolVector3Array = []
+	var batch_size = stream.get_float()
+	for _i in range(batch_size):
+		var x = stream.get_float()
+		var y = stream.get_float()
+		var z = stream.get_float()
+		points.append(Vector3(x, y, z))
+	return points
