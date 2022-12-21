80a46e1b7cf80f6b737d5ec0e29675aebb573fe7
DrHaid <40427630+DrHaid@users.noreply.github.com>
2022-12-21 11:56:44 +0100

    draw landmarks as spheres

--- udp_client.gd
+++ udp_client.gd
@@ -14,18 +14,12 @@ func _process(_delta):
 		var stream = StreamPeerBuffer.new()
 		stream.data_array = socketUDP.get_packet()
 		var batch_size = stream.get_float()
-		for i in range(batch_size):
+		for _i in range(batch_size):
 			var x = stream.get_float()
 			var y = stream.get_float()
 			var z = stream.get_float()
 			points.append(Vector3(x, y, z))
 		
-#		var msg_raw = socketUDP.get_packet().get_string_from_utf8()
-#		var arr = msg_raw.split("\n")
-#		for msg in arr:
-#			var parts = msg.split(";")
-#			if parts.size() == 4:
-#				points.append(Vector3(parts[1], float(parts[2]) * -1, parts[3]))
 		emit_signal("face_mesh_received", points)
 
 func _exit_tree():
